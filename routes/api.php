d403475f2b8f891be0d35ac14c595c8432bf3aeb
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-03-21 01:32:29 +0200

    add route to showing user into api routing page

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/users', 'UsersApiController@index');
+Route::get('/users/{id}', 'UsersApiController@show');
