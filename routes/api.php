072bf14a2d555cfd783a266d827affd1188fb728
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-03-21 01:37:25 +0200

    add user edit route to api routing file

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/users', 'UsersApiController@index');
-Route::get('/users/{id}', 'UsersApiController@show');
+Route::get('/users/{user}', 'UsersApiController@show');
+Route::get('/users/{user}/edit', 'UsersApiController@edit');
