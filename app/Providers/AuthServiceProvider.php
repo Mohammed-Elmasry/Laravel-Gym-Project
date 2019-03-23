f3647b1ddaa3f752625f9e74bbf0d32834deaa77
Mohammed Elmasry <mohammed.hussein.mahmoud@gmail.com>
2019-03-23 20:06:25 +0200

    add AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -20,6 +21,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Passport::routes();
         $this->registerPolicies();
     }
 }
