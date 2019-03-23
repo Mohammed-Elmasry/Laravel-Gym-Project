b906cc9acd29a76ddb2cee5913c5df8804742d7f
Rawda Sherif <rawdasherif119@gmail.com>
2019-03-23 16:48:52 +0200

    add countries package

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        'Webpatser\Countries\CountriesServiceProvider',
 
         /*
          * Package Service Providers...
@@ -225,7 +226,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Countries' => 'Webpatser\Countries\CountriesFacade',
 
     ],
 
+   
 ];
